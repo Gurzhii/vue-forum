d7b196008a2dd86ffb99b7be40cecf70648464bf
Gurzhii Dmitry <gurzhii.dmitriy@gmail.com>
2020-04-29 16:24:27 +0300

    wip && router

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,61 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <div :key="thread.key" v-for="thread in threads" class="col-large push-top">
+      <h1>{{ thread.title }}</h1>
+      <div class="post-list">
+        <div class="post">
+          <div class="user-info">
+            <a href="#" class="user-name">Joseph Kerr</a>
+            <a href="#">
+              <img class="avatar-large" src="https://www.sideshowtoy.com/photo_9030371_thumb.jpg" alt />
+            </a>
+            <p class="desktop-only text-small">116 posts</p>
+          </div>
+          <div class="post-content">
+            <div>
+              <blockquote class="small">
+                <div class="author">
+                  <a href="/user/robin" class>robin</a>
+                  <span class="time">a month ago</span>
+                  <i class="fa fa-caret-down"></i>
+                </div>
+                <div class="quote">
+                <p>
+                  Is horseradish and Wasabi the same thing? I've heard so many different things.
+                </p>
+                </div>
+              </blockquote>
+              <p>They're not the same!</p>
+            </div>
+          </div>
+          <div class="post-date text-faded">6 hours ago</div>
+        </div>
+      </div>
+    </div>
   </div>
+  <!-- <div>
+    <h1>Welcome to the forum</h1>
+    <div :key="thread.key" v-for="thread in threads">
+      <h2>{{ thread.title }}</h2>
+      <div :key="postId" v-for="postId in thread.posts">
+        <p>{{ users[posts[postId].userId].name }}</p>
+        <p>{{ posts[postId].text }}</p>
+      </div>
+    </div>
+  </div> -->
 </template>
 
 <script>
+import { threads, posts, users } from '@/data';
+
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String,
+  data() {
+    return {
+      threads,
+      posts,
+      users,
+    };
   },
 };
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
