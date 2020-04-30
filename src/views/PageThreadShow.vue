518659197d9ea6999c0d8629be7f42581dd6dee2
Gurzhii Dmitry <gurzhii.dmitriy@gmail.com>
2020-04-30 14:34:24 +0300

    add post feature

--- src/views/PageThreadShow.vue
+++ src/views/PageThreadShow.vue
@@ -2,11 +2,24 @@
   <div class="col-large push-top">
     <h1>{{ thread.title }}</h1>
     <PostListComponent :posts="posts"/>
+    <form @submit.prevent="addPost">
+      <div class="form-group">
+        <textarea
+          v-model="newPostText"
+          cols="30"
+          rows="10"
+          class="form-input"
+        ></textarea>
+      </div>
+      <div class="form-actions">
+        <button class="btn-blue">Submit</button>
+      </div>
+    </form>
   </div>
 </template>
 
 <script>
-import { threads, posts } from '@/data';
+import { threads, posts, users } from '@/data';
 import PostListComponent from '@/components/PostListComponent.vue';
 
 export default {
@@ -22,12 +35,29 @@ export default {
   data() {
     return {
       thread: threads[this.id],
+      newPostText: '',
     };
   },
   computed: {
     posts() {
       const postIds = Object.values(this.thread.posts);
       return Object.values(posts).filter((post) => postIds.includes(post['.key']));
+    },
+  },
+  methods: {
+    addPost() {
+      const postId = `greatPost${Math.random()}`;
+      const post = {
+        text: this.newPostText,
+        publishedAt: Math.floor(Date.now() / 1000),
+        threadId: this.id,
+        userId: 'Miej9zSGMRZKDvMXzfxjVOyv3RF3',
+        '.key': postId,
+      };
+      this.$set(posts, postId, post);
+      this.$set(this.thread.posts, postId, postId);
+      this.$set(users[post.userId].posts, postId, postId);
+      this.newPostText = '';
     },
   },
 };
